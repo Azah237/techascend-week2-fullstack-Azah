58a86ca5a67a98700df341afbe199c8158416196
Azah237 <sheyrhonazah@gmail.com>
2026-03-15 21:19:24 +0100

    linked script.js to index.html to allow the button element to be created before the script runs

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
-document.getElementById("myButton").addEventListener("click", function() {
-    document.getElementById("myText").style.display = "block";
-});
+/*button.addEventListener("click", function() {
+    document.getElementById("myText").textContent = "Welcome to TechAscend Cohort 2.";
+});*/
+
+// document.getElementById("myButton").addEventListener("click", function() {
+//     document.getElementById("myText").style.display = "block";
+// });
+
+
+function showText() {
+  document.getElementById("myText").style.display = "block";
+}
+
+document.getElementById("myButton").addEventListener("click", showText);
